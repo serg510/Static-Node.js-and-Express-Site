d055e7b90bbe0e4b66c082de11ee5797aca111e0
sergio <sbarajas510@gmail.com>
2020-06-22 23:56:28 -0700

    added images/ for projects

--- app.js
+++ app.js
@@ -6,10 +6,16 @@ const app = express();
 // view engine setup
 app.use("view engine","pug");
 //static route to serve the static files located in the public folder
-app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use('/static', express.static('public'));
+
+//An "index" route (/) to render the "Home" page with the locals set to data.projects
+app.get('/', (req,res) => {
+    res.locals.projects = data.projects;
+    res.render('index');
+});
+app.get('/about', (req,res) => {
+    res.render('about');
+});
 
 
 
-
-
-module.exports = 
