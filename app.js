cf70218594ddb96b433138d7635eb1c3687c8ef9
sergio <sbarajas510@gmail.com>
2020-06-24 23:56:26 -0700

    import JSON data

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
-const jsonData = require("./data/data.json");
+const { projects } = require("./data/data.json");
+ 
 
 const app = express();
 // view engine setup
