4d4bd491441f4732f5e2cee926f703d3f551743c
sergio <sbarajas510@gmail.com>
2020-06-27 23:13:40 -0700

    set-up server

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
 const express = require("express");
 const path = require("path");
-const { projects } = require("./data/data.json");
+
  
 
 const app = express();
 // view engine setup
-app.use("view engine","pug");
+app.set("view engine","pug");
 //static route to serve the static files located in the public folder
-app.use('/static', express.static('public'));
 
-//An "index" route (/) to render the "Home" page with the locals set to data.projects
-app.get('/', (req,res) => {
-    res.locals.projects = data.projects;
-    res.render('index');
+app.use('/static', express.static(path.join(__dirname, 'public')))
+
+const mainRoute = require("./routes/index.js");
+const aboutRoute = require("./routes/about.js");
+const projectRoute = require("./routes/project.js");
+
+app.use(mainRoute);
+app.use('/about', aboutRoute);
+app.use('/project', projectRoute);
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`The server is running on localhost:${PORT}!`);
 });
-app.get('/about', (req,res) => {
-    res.render('about');
-});
-
-
 
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const data = require("./data/data.json")
+const data = require("../data/data.json")
 
 
 //An "index" route (/) to render the "Home" page with the locals set to data.projects
