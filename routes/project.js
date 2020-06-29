313bb56398cb8befc27769c68d567303845451fa
sergio <sbarajas510@gmail.com>
2020-06-28 23:53:01 -0700

    fixed routes

--- routes/project.js
+++ routes/project.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const { projects } = require("../data/data.json")
 
 
-
-router.get('/projects/:id', function(req, res, next) {
+router.get('/:id', function(req, res, next) {
   const projectId = req.params.id;
   const project = projects.find( ({ id }) => id === +projectId );
   
