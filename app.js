6e24614f01c78f9aadf0b572dc66e2b20bc3724f
sergio <sbarajas510@gmail.com>
2020-06-30 21:18:50 -0700

    added an "error" and a "error-handler"

--- app.js
+++ app.js
@@ -18,6 +18,20 @@ app.use(mainRoute);
 app.use('/about', aboutRoute);
 app.use('/project', projectRoute);
 
+//create error 
+app.use((req, res, next) => {
+    const err = new Error('Page Not Found');
+    err.status = 404;
+    next(err);
+  });
+
+//error handler to handle the error 
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error');
+});
+
 
 const PORT = process.env.PORT || 3000;
 
